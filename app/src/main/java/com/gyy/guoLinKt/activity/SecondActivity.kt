19740b16bdf521682685792666032c56b743a7f4
felicity520 <51052978+felicity520@users.noreply.github.com>
2020-09-03 23:18:22 +0800

    提交学习toolbar和DrawerLayout

--- app/src/main/java/com/gyy/guoLinKt/activity/SecondActivity.kt
+++ app/src/main/java/com/gyy/guoLinKt/activity/SecondActivity.kt
@@ -5,22 +5,40 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.gyy.guoLinKt.R
-import com.gyy.guoLinKt.bean.Fruit
+import com.gyy.guoLinKt.kotlin.showToast
 import kotlinx.android.synthetic.main.activity_second.*
 
+/***
+ * 1、学习Material Design
+ * 2、接收序列化数据
+ * 3、action跳转
+ * 4、接收Activity跳转带Extra参数的
+ * 5、回传到上一个activity的数据setResult
+ */
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        加上这句toolbar才会显示出来,加载id
+        setSupportActionBar(my_toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
 //        val keyvalue = intent.getStringExtra("KEY")
 //        Log.e("gyy", "keyvalue is $keyvalue")
 
+//        验证序列号跳转的
 //        val fruit = intent.getSerializableExtra("serial_data") as Fruit
-        val fruit = intent.getParcelableExtra("serial_data") as Fruit
-        Log.e(TAG, "onCreate: fruit:${fruit.name}")
+//        val fruit = intent.getParcelableExtra("serial_data") as Fruit
+//        Log.e(TAG, "onCreate: fruit:${fruit.name}")
 
         button2.setOnClickListener {
             val intent = Intent()
@@ -29,6 +47,22 @@ class SecondActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.backup -> "backup".showToast()
+            R.id.delete -> "delete".showToast()
+            R.id.settings -> "settings".showToast()
+            android.R.id.home -> drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
     }
 
     companion object {
